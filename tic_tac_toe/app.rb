ac51ed1d7b5b1e2181d06d31b99f6cc94fba9ce9
Andrew DeNike <denikeandrew@gmail.com>
2017-09-29 12:20:22 -0700

    Add TTT class

--- tic_tac_toe/app.rb
+++ tic_tac_toe/app.rb
@@ -1,7 +1,31 @@
 class TicTacToe
+	def initialize
+		@board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+	end 
+
+	WIN_COMBINATIONS = [
+		[0,1,2], #top row
+		[3,4,5], #middle row
+		[6,7,8], #bottom row
+		[0,3,6], #left column
+		[1,4,7], #middle column
+		[2,5,8], #right column
+		[0,4,8], #diagonal 
+		[2,4,6] #other diagonal
+	]
+
+	def display_board(board)
+	  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+	  puts "-----------"
+	  puts "#{board[3]}|#{board[4]}|#{board[5]}"
+	  puts "-----------"
+	  puts "#{board[6]}|#{board[7]}|#{board[8]}"
+	end 
+
 
 end
 
+#____________________________________________________
 
 def display_board(board)
   puts "#{board[0]}|#{board[1]}|#{board[2]}"
@@ -11,13 +35,13 @@ def display_board(board)
   puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end 
 
-#def input_to_index(userinput)
-	#index = (userinput.to_i) - 1
-#end 
+def input_to_index(input = gets.strip)
+	(input.to_i) - 1
+end 
 
 def turn(board)
 	puts "Where would you like to go?  Please enter 1-9:"
-	input = (gets.strip.to_i) - 1
+	input = input_to_index()
 	if valid_move?(board, input)
 		move(board, input, " X ")
 		display_board(board)
@@ -51,6 +75,7 @@ def play(board)
 		turnCount += 1 
 	end 
 end 
+
 
 
 
